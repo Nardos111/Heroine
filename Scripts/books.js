50cfba917c63e1bdf14f5af5462f5ad30906a54c
nardos111 <nardos2alemu@gmail.com>
2021-04-25 19:05:54 +0300

    Finalize books page

--- Scripts/books.js
+++ Scripts/books.js
@@ -10,9 +10,12 @@
         for (var i = 1; i < 4; i++) {
             itemToClone = itemToClone.next();
 
+            // wrap around if at end of item collection
             if (!itemToClone.length) {
                 itemToClone = $(this).siblings(":first");
             }
+
+            // grab item, clone, add marker class, add to collection
             itemToClone
                 .children(":first-child")
                 .clone()
